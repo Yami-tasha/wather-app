309938aca471e9076128c6a696cc9a2750aa50f6
Yami-tasha <113445980+Yami-tasha@users.noreply.github.com>
2022-09-28 11:17:29 +0100

    Display the forecast data using the API

--- src/app.js
+++ src/app.js
@@ -14,25 +14,37 @@ return `${day} ${hours}:${minutes}`
 }
 
 // week 8
+function formatDay(timestamp){
+let date = new Date(timestamp * 1000);
+let day = date.getDay();
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+return days[day];
+}
+
 function displayForecast(response) {
-    console.log(response.data.daily);
+let forecast = response.data.daily;
 let forecastElement = document.querySelector("#forecast");
 
 let forecastHTML = `<div class="row">`;
-let days = [ "Tue", "Wed", "Thu", "Fri"];
-days.forEach(function(day){
+
+forecast.forEach(function(forecastDay, index){
+    if (index < 6){
     forecastHTML = forecastHTML + `
     <div class="col-2">
-        <div class="weather-foecast-date">${day}</div>
-        <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+        <div class="weather-foecast-date">${formatDay(forecastDay.dt)}</div>
+        <img  src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
          alt="" width="50">
          <div class="weather-foecast-temperature">
             <span class="weather-foecast-temperature-max">
-         24°</span>
+         ${Math.round(forecastDay.temp.max)}°</span>
          <span class="weather-foecast-temperature-min">
-         16°</span>
+         ${Math.round(forecastDay.temp.min)}°</span>
         </div>
     </div>`;
+}
 });
 
 forecastHTML = forecastHTML + `</div>`;
@@ -41,7 +53,7 @@ forecastElement.innerHTML = forecastHTML;
 
 function getForecast(coordinates) {
     console.log(coordinates);
-    let apiKey = "a0d03cacc138649973b20df80763e262";
+    let apiKey = "a43564c91a6c605aeb564c9ed02e3858";
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayForecast);
 }
